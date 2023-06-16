afe67ab732be469fe880974eb72fb4b6cee344e6
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-06-16 11:30:54 +0900

    [PGS] 올바른 괄호 / LV2
    https://school.programmers.co.kr/learn/courses/30/lessons/12909

---
+++ bingomangsoo/Lv2_올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        
+        Stack<String> st = new Stack<>();
+		
+		boolean b = true;
+
+		int l = s.length();
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				st.add(String.valueOf(s.charAt(i)));
+			} else {
+				if (st.empty()) {
+					b = false;
+                    break;
+				}else {  
+				    st.pop();
+                }
+			}
+		}
+		if (!st.empty()) {
+			b = false;
+		}
+        return b;
+    }
+}
