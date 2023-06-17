e0c2aca18485787eba8c2d5f93622c160447900b
JWY <107178253+jwyong4@users.noreply.github.com>
2023-06-17 22:40:04 +0900

    [PGS] 땅따먹기 / LV2

---
+++ JWY/PGS_LV2_땅따먹기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][3]);
+            land[i][1] += Math.max(Math.max(land[i - 1][0], land[i - 1][2]), land[i - 1][3]);
+            land[i][2] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][3]);
+            land[i][3] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][2]);
+        }
+        
+        int[] temp = land[land.length - 1];
+        Arrays.sort(temp);
+        answer = temp[temp.length - 1];
+        
+        return answer;
+    }
+}
